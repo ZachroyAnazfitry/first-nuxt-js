2f48dbedea1637c6205c319e9b7a694d5c728656
ZachAnaz <zachroy04@yahoo.com>
2024-02-18 09:47:59 +0800

    routing,layouts,styling with tailwind

---
+++ pages/employee/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Employee number is {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- testing navigation power of Nuxt -->
+    <!-- this header had been moved to default.vue -->
+    <!-- <header>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/about">About using NuxtLink</NuxtLink>
+                </li>
+                <li>
+                    <a href="/about">About using a tag</a>
+                </li>
+            </ul>
+        </nav>
+    </header> -->
+    <div>
+        <p>Welcome Z</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
